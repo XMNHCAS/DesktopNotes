a5d25586f73b6505f0d82b308f017bfcafdc9901
XMNHCAS <1351669870@qq.com>
2023-02-07 17:08:26 +0800

    2023-02-07 by xmnh

--- DesktopNotes/ResourceDictionary/ColorResource.cs
+++ DesktopNotes/ResourceDictionary/ColorResource.cs
@@ -13,8 +13,74 @@ namespace DesktopNotes.ResourceDictionary
 
     public enum ColorResource
     {
-        Red = 0xffffff,
-        Theme
-        Theme_Red
+        // 黄色
+        // #FFF7D1
+        Yellow_Theme = 0xFFF7D1,
+        // #FFE66E
+        Yellow_Option = 0xFFE66E,
+        // #EED767
+        Yellow_Hover = 0xEED767,
+        // #FFF2AB
+        Yellow_Title = 0xFFF2AB,
+
+        // 绿色
+        // #E4F9E0
+        Green_Theme = 0xE4F9E0,
+        // #CBF1C4
+        Green_Option = 0xCBF1C4,
+        // #96DF91
+        Green_Hover = 0x96DF91,
+        // #A1EF9B
+        Green_Title = 0xA1EF9B,
+
+        // 粉色
+        // #FFE4F1
+        Pink_Theme = 0xFFE4F1,
+        // #FFAFDF
+        Pink_Option = 0xFFAFDF,
+        // #EEA3D0
+        Pink_Hover = 0xEEA3D0,
+        // #FFCCE5
+        Pink_Title = 0xFFCCE5,
+
+        // 紫色
+        // #F2E6FF
+        Purple_Theme = 0xF2E6FF,
+        // #D7AFFF
+        Purple_Option = 0xD7AFFF,
+        // #C9A3EE
+        Purple_Hover = 0xC9A3EE,
+        // #E7CFFF
+        Purple_Title = 0xE7CFFF,
+
+        // 蓝色
+        // #E2F1FF
+        Blue_Theme = 0xE2F1FF,
+        // #9EDFFF
+        Blue_Option = 0x9EDFFF,
+        // #93D0EE
+        Blue_Hover = 0x93D0EE,
+        // #CDE9FF
+        Blue_Title = 0xCDE9FF,
+
+        // 灰色
+        // #F3F2F1
+        Grey_Theme = 0xF3F2F1,
+        // #E0E0E0
+        Grey_Option = 0xE0E0E0,
+        // #D1D1D1
+        Grey_Hover = 0xD1D1D1,
+        // #E1DFDD
+        Grey_Title = 0xE1DFDD,
+
+        // 炭笔
+        // #696969
+        Charcoal_Theme = 0x696969,
+        // #767676
+        Charcoal_Option = 0x767676,
+        // #7F7F7F
+        Charcoal_Hover = 0x7F7F7F,
+        // #494745
+        Charcoal_Title = 0x494745,
     }
 }
