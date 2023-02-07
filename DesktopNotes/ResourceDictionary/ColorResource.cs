466548d56e76db002c1f4c1fcf4f01f9e1291808
XMNHCAS <1351669870@qq.com>
2023-02-07 16:27:10 +0800

    添加项目文件。

---
+++ DesktopNotes/ResourceDictionary/ColorResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopNotes.ResourceDictionary
+{
+    //public class ColorResource
+    //{
+
+    //}
+
+    public enum ColorResource
+    {
+        Red = 0xffffff,
+        Theme
+        Theme_Red
+    }
+}
